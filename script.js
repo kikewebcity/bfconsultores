8ad2d54af58fc4b4f9338f3df0c36a49c0580bba
kikewebcity <kikeadomicilio@gmail.com>
2024-12-30 15:03:18 -0600

    venrsion6

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+//funcion animar contador 
 $(document).ready(function(){
   $('.counter').counterUp({
     delay: 10,
     time: 1200
   });
 });
+
+//funcion animar texto de video 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.overlay h3, .overlay p, .overlay .btn');
+  elements.forEach(el => {
+    el.style.opacity = '0';
+  });
+
+  window.setTimeout(() => {
+    elements.forEach(el => {
+      el.style.opacity = '';
+    });
+  }, 100);
+});
+
+
+
+
+
+
+
+
